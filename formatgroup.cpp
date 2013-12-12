7643c58aa0dd1f3b3dc40069120e9fe589091296
Tony Camuso <tcamuso@redhat.com>
2013-12-12 14:30:43 -0500

    project-wide: Insert/update GPL license and copyright notification.
    Signed-off-by: Tony Camuso <tony@camusofamily.com>

--- formatgroup.cpp
+++ formatgroup.cpp
@@ -1,3 +1,27 @@
+/******************************************************************************
+**
+**  bits - an application to arithmeticall and logically combine and
+**         manipulate bit patterns and their corresponding hex numbers.
+**
+**  Tony Camuso
+**  Created December, 2011
+**
+**    bits (bitview) is free software. You can redistribute it and/or modify
+**    it under the terms of the GNU General Public License as published by
+**    the Free Software Foundation, either version 3 of the License, or
+**    (at your option) any later version.
+**
+**    This program is distributed in the hope that it will be useful,
+**    but WITHOUT ANY WARRANTY; without even the implied warranty of
+**    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**    GNU General Public License for more details.
+**
+**  GNU General Public License http://www.gnu.org/licenses/gpl.html
+**
+**  Copyright (c) 2011-2014 by Tony Camuso.
+**
+******************************************************************************/
+
 #include "formatgroup.h"
 
 FormatGroup::FormatGroup(QWidget *parent) :
